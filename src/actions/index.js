127e36c38a8dcd340731503e0a31c3d7efa63b0a
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-05-10 08:20:12 +0000

    Done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,18 @@
-let counter = 1;
-
-export function addMovie(movie) {
-  movie.id = ++counter;
+export function fetchPets() {
+  const pets = [
+    {id: 1, name: 'Grover', description: 'A Furry Blue Guy who is very cute'},
+    {id: 2, name: 'Fido', description: 'A pretty normal looking dog'},
+    {id: 3, name: 'Sparky', description: 'Orange cat with a laid back attitude'}
+  ];
   return {
-    type: 'ADD_MOVIE',
-    movie
+    type: 'FETCH_PETS',
+    pets
   };
 }
+
+export function addPet(pet){
+  return {
+    type: 'ADD_PET',
+    pet
+  };
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import NavBar from './NavBar';
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
+import PetsPage from '../containers/PetsPage';
 
-export default (props) => {
-  return (
+const App = (props) => 
+  <Router>
     <div>
-      < NavBar title="Nested Routes!"/>
+      <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
+        <NavLink style={{ marginRight: '10px' }} to="/pets">See All The Pets!</NavLink>
+        <NavLink style={{ marginRight: '10px' }} to="/pets/new">Add A Pet</NavLink>
+      </div>
+      <Route exact path="/" render={() => <h3>Welcome to the Pets List App</h3>} />
+      <Route path="/pets" component={PetsPage} />
     </div>
-  )
-}
+  </Router>;
+
+export default App;
