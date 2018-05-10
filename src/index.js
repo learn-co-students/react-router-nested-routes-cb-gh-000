533e79b44802ea9979b504df6fc99bb3a5d2477a
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-05-10 08:26:50 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import rootReducer from './reducers'
+import { render } from 'react-dom';
+import store from './configureStore';
 import { Provider } from 'react-redux';
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
-
 import App from './components/App';
-import MoviesPage from './containers/MoviesPage';
-import MoviesNew from './containers/MoviesNew';
-import MoviesAbout from './components/MoviesAbout';
-import MoviesShow from './containers/MoviesShow'
 
-const initialState = {movies: [ { id: 1, title: 'A River Runs Through It' } ]}
-
-const store = createStore(rootReducer, initialState);
-
-ReactDOM.render(
-  (<Provider store={store} >
-    <Router history={browserHistory} >
-      <Route path="/" component={App} />
-      <Route path='/movies' component={MoviesPage}/>
-    </Router>
-  </Provider>),
-document.getElementById('container'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
